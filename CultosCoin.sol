08a730710c947cd6b5d3f77cc9aec202019a568e
thanasik <6022002+thanasik@users.noreply.github.com>
2022-05-19 19:15:18 -0400

    Put capped supply back

--- CultosCoin.sol
+++ CultosCoin.sol
@@ -3,14 +3,23 @@ pragma solidity >=0.8.8 <0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Cultos is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("Cultos", "CULTOS") {
-        _mint(msg.sender, 1000000000 * 10**decimals());
+contract Cultos is ERC20, ERC20Burnable, ERC20Capped, Ownable {
+    constructor()
+        ERC20("Cultos", "CULTOS")
+        ERC20Capped(1000000000 * (10**decimals()))
+        ERC20Burnable()
+    {
+        _mint(msg.sender, 1000000000);
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function _mint(address account, uint256 supply_)
+        internal
+        virtual
+        override(ERC20, ERC20Capped)
+    {
+        super._mint(account, supply_ * (10**decimals()));
     }
 }
